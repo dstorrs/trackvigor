500eb9f67901f2e205d31fdfc1c2046a8bd4adba
David K. Storrs <david.storrs@gmail.com>
2013-02-20 16:54:24 -0500

    Server renders as HTML, not text/plain

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ var port = 7777;
 function onRequest(req, resp){
 	path = url.parse(req.url).pathname;
 	if ( path != '/favicon.ico' ) { console.log("Routing a request for: " + path); }
-	var body = 'hello world';
+	var body = '<h1>hello world</h1>';
 	resp.writeHead(
 		200,
 		{
 			'Content-Length': body.length,
-			'Content-Type': 'text/plain'
+			'Content-Type': 'text/html'
 		}
 	);
 	resp.end(body);
