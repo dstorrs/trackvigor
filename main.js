9b99a9c517bb05f478c0120bc0f090e33ab91d2e
David K. Storrs <david.storrs@gmail.com>
2013-02-20 17:10:26 -0500

    Refactored the routing table stuff into app/router.js

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
 http = require('http');
 url = require('url');
+router = require('app/router');
 
 var port = 7777;
-
-var routing_table = {};
-
-var defaultHandler = function(req, resp) {
-	var body = '<h1>The page you requested could not be found</h1>';
-	resp.writeHead(
-		200,
-		{
-			'Content-Length': body.length,
-			'Content-Type': 'text/html'
-		}
-	);
-	resp.end(body);
-}
-routing_table['default_handler'] = defaultHandler;
 
 function onRequest(req, resp){
 	path = url.parse(req.url).pathname;
 	if ( path != '/favicon.ico' ) { console.log("Routing a request for: " + path); }
-	(routing_table['default_handler'])(req, resp);
+	router.get_handler(path)(req, resp);
 }
 
 http.createServer(onRequest).listen(port);
