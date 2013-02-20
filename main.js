260f99342e2767f9b8fd12bf6702a5e4651e52ec
David K. Storrs <david.storrs@gmail.com>
2013-02-20 17:01:51 -0500

    Added basic routing table to main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ url = require('url');
 
 var port = 7777;
 
-function onRequest(req, resp){
-	path = url.parse(req.url).pathname;
-	if ( path != '/favicon.ico' ) { console.log("Routing a request for: " + path); }
-	var body = '<h1>hello world</h1>';
+var routing_table = {};
+
+var defaultHandler = function(req, resp) {
+	var body = '<h1>The page you requested could not be found</h1>';
 	resp.writeHead(
 		200,
 		{
@@ -15,6 +15,13 @@ function onRequest(req, resp){
 		}
 	);
 	resp.end(body);
+}
+routing_table['default_handler'] = defaultHandler;
+
+function onRequest(req, resp){
+	path = url.parse(req.url).pathname;
+	if ( path != '/favicon.ico' ) { console.log("Routing a request for: " + path); }
+	(routing_table['default_handler'])(req, resp);
 }
 
 http.createServer(onRequest).listen(port);
