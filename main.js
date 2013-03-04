a4eef0f2e657bc52910452d8e4e7ddcdefc114f5
David K. Storrs <david.storrs@gmail.com>
2013-03-04 12:40:17 -0800

    Moved to using node-static

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-http = require('http');
-url = require('url');
-router = require('app/router');
+var http = require('http');
+var url = require('url');
+var static = require('node-static');
+var u = require('app/util');
 
 var port = 7777;
+var file = new(static.Server)(__dirname + '/templates/assets');
 
-function onRequest(req, resp){
-	path = url.parse(req.url).pathname;
-	if ( ! path.match(/\/favicon\./) ) { console.log("Routing a request for: " + path); }
+function get_url_path(req) { return url.parse(req.url).pathname }
 
-	router.get_handler(path)(req, resp);
-}
+http.createServer(function(req, res) {
+	if ( ! get_url_path( req ).match(/\/favicon\./) ) { u.log("Routing a request for: " + get_url_path( req )); }
 
-http.createServer(onRequest).listen(port);
+	//    Add the listeners
+	req.addListener(
+		'end',
+		function() {
+			file.serve(
+				req,
+				res,
+				function(err, result) {
+					//    File not found?
+					if (err && (err.status === 404)) {
+						file.serveFile('/html/404.html', 404, {}, req, res);
+					}
+				}
+			)
+		}
+	);
+}).listen(port);
 
-console.log("\n\tSTART: ");
-console.log(new Date());
-console.log("Server started and listening on port " + port + "...\n");
+console.log("\n");
+u.log("\tSTART ");
+u.log(new Date());
+u.log("Server started and listening on port " + port + "...\n");
